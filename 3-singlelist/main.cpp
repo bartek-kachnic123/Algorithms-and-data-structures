d3e9e640f937e40e7bf2982aac0da60a729f6fb9
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-11-13 18:04:19 +0100

    copy constructor and assigment added

--- 3-singlelist/main.cpp
+++ 3-singlelist/main.cpp
@@ -1,37 +1,41 @@
 #include "singlelist.h"
 
-void testCopy_constructor() {
-    SingleList<int> sList;
-    for (int i=1; i < 10; ++i)
+void testCopy_constructor()
+{
+    SingleList<int> sList, sList2;
+    for (int i = 1; i < 10; ++i)
     {
         sList.push_front(i);
     }
-
-    SingleList<int> sList2(sList);
-    std::cout << "sList 1: ";
+    std::cout << "Copy constructor test: " << std::endl;
+    std::cout << "sList : ";
     sList.display();
+    SingleList<int> emptyList(sList2);
+    SingleList<int> sList3(sList);
+    std::cout << "sList3 : ";
+    sList3.display();
     std::cout << std::endl;
+}
 
-    std::cout << "sList 2: ";
+void testCopy_assigment_operator()
+{
+    SingleList<int> sList, sList2;
+    for (int i = 1; i < 10; ++i)
+    {
+        sList.push_front(i);
+    }
+    sList2 = sList;
+    std::cout << "Copy assigment operator test: " << std::endl;
+    std::cout << "sList: ";
+    sList.display();
+    std::cout << "sList2: ";
     sList2.display();
     std::cout << std::endl;
-
-
 }
 int main()
 {
+    testCopy_assigment_operator();
     testCopy_constructor();
-
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
--- 3-singlelist/singlelist.h
+++ 3-singlelist/singlelist.h
@@ -3,67 +3,105 @@
 #ifndef SINGLELIST_H
 #define SINGLELIST_H
 
-#include <iostream>     // deklaracje strumieni cout, cin, cerr
-#include <cassert>    // assert()
+#include <iostream> // deklaracje strumieni cout, cin, cerr
+#include <cassert>  // assert()
 
 template <typename T>
-struct SingleNode {
-// the default access mode and default inheritance mode are public
+struct SingleNode
+{
+    // the default access mode and default inheritance mode are public
     T value;
     SingleNode *next;
     SingleNode() : value(T()), next(nullptr) {} // konstruktor domyslny
-    SingleNode(const T& item, SingleNode *ptr=nullptr) : value(item), next(ptr) {}
+    SingleNode(const T &item, SingleNode *ptr = nullptr) : value(item), next(ptr) {}
     ~SingleNode() {} // destruktor
 };
 
 template <typename T>
-class SingleList {
+class SingleList
+{
     SingleNode<T> *head, *tail;
+
 public:
     SingleList() : head(nullptr), tail(nullptr) {}
     ~SingleList(); // tu trzeba wyczyscic wezly
 
-    SingleList(const SingleList& other); // copy constructor
+    SingleList(const SingleList &other) // copy constructor
+    {
+        if (other.head == nullptr)
+        {
+            head = tail = nullptr;
+        }
+        else
+        {
+            head = tail = nullptr;
+            SingleNode<T> *node = other.head;
+            while (node)
+            {
+                push_back(node->value);
+                node = node->next;
+            }
+        }
+    }
+
     // usage:   SingleList<int> list2(list1);
 
-    SingleList(SingleList&& other); // move constructor NIEOBOWIAZKOWE
+    SingleList(SingleList &&other); // move constructor NIEOBOWIAZKOWE
     // usage:   SingleList<int> list2(std::move(list1));
 
-    SingleList& operator=(const SingleList& other); // copy assignment operator, return *this
+    SingleList &operator=(const SingleList &other) // copy assignment operator, return *this
+    {
+        if (this == &other)
+            return *this;
+
+        clear();
+        head = nullptr;
+        tail = nullptr;
+        SingleNode<T> *node = other.head;
+        while (node)
+        {
+            push_back(node->value);
+            node = node->next;
+        }
+
+        return *this;
+    }
     // usage:   list2 = list1;
 
-    SingleList& operator=(SingleList&& other); // move assignment operator, return *this
+    SingleList &operator=(SingleList &&other); // move assignment operator, return *this
     // usage:   list2 = std::move(list1); NIEOBOWIAZKOWE
 
     bool empty() const { return head == nullptr; }
-    int size() const; // O(n) bo trzeba policzyc
-    void push_front(const T& item); // O(1), L.push_front(item)
-    void push_front(T&& item); // O(1), L.push_front(std::move(item)) NIEOBOWIAZKOWE
-    void push_back(const T& item); // O(1), L.push_back(item)
-    void push_back(T&& item); // O(1), L.push_back(std::move(item)) NIEOBOWIAZKOWE
-    T& front() const { return head->value; } // zwraca poczatek, nie usuwa
-    T& back() const { return tail->value; } // zwraca koniec, nie usuwa
-    void pop_front(); // usuwa poczatek O(1)
-    void pop_back(); // usuwa koniec O(n)
-    void clear(); // czyszczenie listy z elementow O(n)
-    void display(); // O(n)
-    void reverse(); // O(n)
+    int size() const;                        // O(n) bo trzeba policzyc
+    void push_front(const T &item);          // O(1), L.push_front(item)
+    void push_front(T &&item);               // O(1), L.push_front(std::move(item)) NIEOBOWIAZKOWE
+    void push_back(const T &item);           // O(1), L.push_back(item)
+    void push_back(T &&item);                // O(1), L.push_back(std::move(item)) NIEOBOWIAZKOWE
+    T &front() const { return head->value; } // zwraca poczatek, nie usuwa
+    T &back() const { return tail->value; }  // zwraca koniec, nie usuwa
+    void pop_front();                        // usuwa poczatek O(1)
+    void pop_back();                         // usuwa koniec O(n)
+    void clear();                            // czyszczenie listy z elementow O(n)
+    void display();                          // O(n)
+    void reverse();                          // O(n)
 
     // Operacje z indeksami. NIEOBOWIAZKOWE
-    T& operator[](int pos); // podstawienie L[pos]=item, odczyt L[pos]
-    const T& operator[](int pos) const; // dostep do obiektu const
+    T &operator[](int pos);             // podstawienie L[pos]=item, odczyt L[pos]
+    const T &operator[](int pos) const; // dostep do obiektu const
     void erase(int pos);
-    int index(const T& item); // jaki index na liscie (-1 gdy nie ma) O(n)
-    void insert(int pos, const T& item); // inserts item before pos,
-    void insert(int pos, T&& item); // inserts item before pos,
+    int index(const T &item);            // jaki index na liscie (-1 gdy nie ma) O(n)
+    void insert(int pos, const T &item); // inserts item before pos,
+    void insert(int pos, T &&item);      // inserts item before pos,
     // Jezeli pos=0, to wstawiamy na poczatek.
     // Jezeli pos=size(), to wstawiamy na koniec.
 };
 
 template <typename T>
-SingleList<T>::~SingleList() {
+SingleList<T>::~SingleList()
+{
     // I sposob.
-    for (SingleNode<T> *node; !empty(); ) {
+    for (SingleNode<T> *node; !empty();)
+    {
         node = head->next; // zapamietujemy
         delete head;
         head = node; // kopiowanie wskaznika
@@ -73,28 +111,38 @@ SingleList<T>::~SingleList() {
 }
 
 template <typename T>
-void SingleList<T>::push_front(const T& item) {
-    if (!empty()) {
+void SingleList<T>::push_front(const T &item)
+{
+    if (!empty())
+    {
         head = new SingleNode<T>(item, head);
-    } else {
+    }
+    else
+    {
         head = tail = new SingleNode<T>(item);
     }
 }
 
 template <typename T>
-void SingleList<T>::push_back(const T& item) {
-    if (!empty()) {
+void SingleList<T>::push_back(const T &item)
+{
+    if (!empty())
+    {
         tail->next = new SingleNode<T>(item);
         tail = tail->next;
-    } else {
+    }
+    else
+    {
         head = tail = new SingleNode<T>(item);
     }
 }
 
 template <typename T>
-void SingleList<T>::display() {
+void SingleList<T>::display()
+{
     SingleNode<T> *node = head;
-    while (node != nullptr){
+    while (node != nullptr)
+    {
         std::cout << node->value << " ";
         node = node->next;
     }
@@ -102,27 +150,36 @@ void SingleList<T>::display() {
 }
 
 template <typename T>
-void SingleList<T>::pop_front() {
+void SingleList<T>::pop_front()
+{
     assert(!empty());
     SingleNode<T> *node = head; // zapamietujemy
-    if (head == tail) { // jeden wezel na liscie
+    if (head == tail)
+    { // jeden wezel na liscie
         head = tail = nullptr;
-    } else { // wiecej niz jeden wezel na liscie
+    }
+    else
+    { // wiecej niz jeden wezel na liscie
         head = head->next;
     }
     delete node;
 }
 
 template <typename T>
-void SingleList<T>::pop_back() {
+void SingleList<T>::pop_back()
+{
     assert(!empty());
     SingleNode<T> *node = tail; // zapamietujemy
-    if (head == tail) { // jeden wezel na liscie
+    if (head == tail)
+    { // jeden wezel na liscie
         head = tail = nullptr;
-    } else { // wiecej niz jeden wezel na liscie
+    }
+    else
+    { // wiecej niz jeden wezel na liscie
         // Szukamy poprzednika ogona.
-        SingleNode<T> *before=head;
-        while (before->next != tail) {
+        SingleNode<T> *before = head;
+        while (before->next != tail)
+        {
             before = before->next;
         }
         tail = before;
@@ -132,16 +189,11 @@ void SingleList<T>::pop_back() {
 }
 
 template <typename T>
-SingleList<T>::SingleList(const SingleList& other)
+void SingleList<T>::clear()
 {
-    SingleNode<T>* otherHead = other.head;
-    T value;
-    while(otherHead != nullptr)
+    while (!empty())
     {
-        value = otherHead->value;
-        this->push_front(value);
-
-        otherHead = otherHead->next;
+        pop_front();
     }
 }
 
