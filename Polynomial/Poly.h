93d2a36fe08de9b3b30d350a33d422f31e53c859
Bartek Kachnic <bartek.kachnic123@gmail.com>
2023-01-13 10:55:24 +0100

    implemented constructors, destructor and clear method

--- Polynomial/Poly.h
+++ Polynomial/Poly.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <array>
 
 template<typename T>
 class Poly {
@@ -12,8 +13,8 @@ class Poly {
     public:
         // constructors
         Poly();
-        Poly(T... factors); // variadic template
-        Poly(T factors[]);
+        Poly(T factors[], int _size);
+
         // destructor
         ~Poly();
 
@@ -26,20 +27,58 @@ class Poly {
         bool is_zero();
 
         // operators 2 args
-        Poly operator+(const Poly& polynomial);
-        Poly operator-(const Poly& polynomial);
-        Poly& operator*(const Poly& polynomial);
-        bool operator==(const Poly& polynomial);
-        bool operator!=(const Poly& polynomial);
-        Poly& operator=(const Poly& polynomial);
-        Poly& operator=(Poly&& polynomial);
+        Poly<T> operator+(const Poly<T>& polynomial);
+        Poly<T> operator-(const Poly<T>& polynomial);
+        Poly<T>& operator*(const Poly<T>& polynomial);
+        bool operator==(const Poly<T>& polynomial);
+        bool operator!=(const Poly<T>& polynomial);
+        Poly<T>& operator=(const Poly<T>& polynomial);
+        Poly<T>& operator=(Poly<T>&& polynomial);
 
         // operators 1 args
-        T& operator[]();
+        T& operator[](int index);
 
         // friend operators
-        friend std::ostream& operator<<(std::ostream& os, Poly& polynomial);
+        friend std::ostream& operator<<(std::ostream& os, Poly<T>& polynomial);
 
 };
+
+template<typename T>
+Poly<T>::Poly() :
+            size(1)
+            
+{
+    factors = new T[size];
+    assert(factors!=nullptr);
+
+    factors[0] = T();
+}
+
+template<typename T>
+Poly<T>::Poly(T factors[], int _size) :
+            size(_size)          
+{
+    if (size == 0) {
+        Poly();
+    }
+    else {
+        this->factors = new T[size];
+        assert(this->factors!=nullptr);
+        std::copy(factors, factors+size, this->factors);
+    }
+}
+
+
+template<typename T>
+Poly<T>::~Poly() {
+    clear();
+}
+
+template<typename T>
+void Poly<T>::clear() {
+    delete []factors;
+    factors = nullptr;
+}
+
 
 #endif
