a95477379205c9200b86f51b526cb53394221a83
Bartek Kachnic <bartek.kachnic123@gmail.com>
2023-01-13 11:31:07 +0100

    added tests for constructors

--- Polynomial/main.cpp
+++ Polynomial/main.cpp
@@ -1,9 +1,25 @@
 #include "Poly.h"
 
+void test_constructors() {
 
+    std::cout << "Constructors test!" << std::endl;
+    // first 
+    Poly<int> polynomial_1;
+    std::cout << polynomial_1 << std::endl;
+
+
+    // second
+    double arr[] = {1, 2, -3, 4, -5};
+    Poly<double> polynomial_2(arr, sizeof(arr)/sizeof(arr[0]));
+    std::cout << polynomial_2 << std::endl;
+
+    Poly<int> polynomial_3({}, 0);
+
+    std::cout << "Test passed!" << std::endl;
+}
 int main()
 {
-
+    test_constructors();
 
 
 
