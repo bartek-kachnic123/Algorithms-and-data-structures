68f7af21c53a5f6f40a3decb4886f337260bcfdb
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2023-03-06 10:16:17 +0100

    Added preorder, inorder, postorder, remove and more funcs

--- 10-BST/BinarySearchTree.h
+++ 10-BST/BinarySearchTree.h
@@ -2,6 +2,7 @@
 #define BINARY_SEARCH_TREE
 #include <iostream>
 #include <cassert>
+#include <stack>
 
 // Szablon dla drzewa BST.
 template <typename T>
@@ -29,6 +30,7 @@ public:
     T& top() { assert(root != nullptr); return root->value; } // podgląd korzenia
     void insert(const T& item) { root = insert(root, item); }
     void remove(const T& item); // usuwanie przez złączanie
+    BSTNode<T> * remove(BSTNode<T> *node);
     // Szukając element dostajemy wskaźnik do elementu lub nullptr.
     T* search(const T& item) const {
         auto ptr = search(root, item);
@@ -62,8 +64,7 @@ public:
     // Mogą działać na poddrzewie.
     void clear(BSTNode<T> *node);
     BSTNode<T> * insert(BSTNode<T> *node, const T& item); // zwraca nowy korzeń
-    BSTNode<T> * search(BSTNode<T> *node, const T& item) const; // zwraca węzeł
-    BSTNode<T> * remove(BSTNode<T> *node); // zwraca nowy korzeń poddrzewa
+    BSTNode<T> * search(BSTNode<T> *node, const T& item) const;
     void preorder(BSTNode<T> *node);
     void inorder(BSTNode<T> *node);
     void postorder(BSTNode<T> *node);
@@ -133,5 +134,103 @@ T * BinarySearchTree<T>::find_max() {
     return wsk;
 }
 
+
+template <typename T>
+void BinarySearchTree<T>::inorder(BSTNode<T> *node) {
+    if (node == nullptr) return;
+    inorder(node->left);
+    visit(node);
+    inorder(node->right);
+}
+
+template <typename T>
+void BinarySearchTree<T>::preorder(BSTNode<T> *node) {
+    if (node == nullptr) return;
+    visit(node);
+    preorder(node->left);
+    preorder(node->right);
+}
+
+template <typename T>
+void BinarySearchTree<T>::postorder(BSTNode<T> *node) {
+    if (node == nullptr) return;
+    postorder(node->left);
+    postorder(node->right);
+    visit(node);
+}
+
+//Przechodzenie przez drzewo w kolejności preorder możemy zaimplementować iteracyjnie za pomocą stosu.
+template <typename T>
+void BinarySearchTree<T>::iter_preorder() {
+    if (root == nullptr) return;
+    std::stack<BSTNode<T>*> S; // wskaźniki do węzłów
+    BSTNode<T> *node = root;
+    S.push(node);
+    while (!S.empty()) {
+        node = S.top(); // podglądamy
+        S.pop();        // usuwamy ze stosu
+        visit(node); // tu jest właściwe przetworzenie węzła
+        if (node->right != nullptr) // najpierw prawe poddrzewo!
+            S.push(node->right);
+        if (node->left != nullptr)
+            S.push(node->left);
+    }
+}
+
+
+template <typename T>
+void BinarySearchTree<T>::remove(const T& item) { // usuwanie przez złączanie
+    // Najpierw znajdujemy wezel i jego rodzica.
+    BSTNode<T> *node = root;
+    BSTNode<T> *prev = nullptr;
+    while (node != nullptr) {
+        if (node->value == item)
+            break;
+        prev = node;
+        if (item < node->value) {
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+    }
+    // Szukamy przyczyny przerwania pętli while.
+    if (node != nullptr && node->value == item) {
+        // node ma być usunięty.
+        if (node == root) {
+            root = remove(node);
+        } else if (prev->left == node) {
+            prev->left = remove(node);
+        } else {
+            prev->right = remove(node);
+        }
+    } else if (root != nullptr) {
+        ; // klucza nie znaleziono
+    } else { // root == nullptr
+        ; // drzewo jest puste
+    }
+}
+
+template <typename T>
+BSTNode<T> * BinarySearchTree<T>::remove(BSTNode<T> *node) {
+    // Mamy usunąć węzeł i zwrócić nowy korzeń poddrzewa.
+    assert(node != nullptr);
+    BSTNode<T> *new_root;
+    if (node->right == nullptr) { // node nie ma prawego dziecka
+        new_root = node->left;
+    } else if (node->left == nullptr) { // node nie ma lewego dziecka
+        new_root = node->right;
+    } else { // obecne lewe i prawe dziecko
+        new_root = node; // zapisujemy stary korzeń
+        node = node->left; // idziemy w lewo
+        while (node->right != nullptr) { // idziemy w prawo do końca
+            node = node->right;
+        }
+        node->right = new_root->right; // prawe poddrzewo przełączone
+        node = new_root; // węzeł do usunięcia
+        new_root = node->left; // nowy korzeń
+    }
+    delete node;
+    return new_root;
+}
 
 #endif // BINARY_SEARCH_TREE
